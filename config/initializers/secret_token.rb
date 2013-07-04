1693a3d7ba5a9ee7840cabcad42bf558d7a196e4
emAlexanderFisenko/em <em1alexanderfisenko@gmail.com/em>
2013-07-04 12:45:55 +0600

    commiting

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+M::Application.config.secret_token = 'caf2ea4ebfc647dacd9629838e90ac4cc86373ec179af1fd9e620c0d118833d886b876e785d79c453a5bae5ef46f035014c547a7236c007b0536d6e096b52799'
